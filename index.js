9e0e0201c7eac505a3b177cbb378a21f55131d4b
Abhishek joshi <107804782+Abhishek78394@users.noreply.github.com>
2023-04-01 00:28:23 +0530

    second

--- index.js
+++ index.js
@@ -6,11 +6,24 @@ var cors = require('cors')
 const PORT = process.env.PORT || 4000;
 const connectDatabase = require("./models/db");
 const indexRouter  =  require('./routes/indexRoute')
- 
-app.use(cors());
-connectDatabase();
+const path = require("path");
+
+const corsOptions = {
+    origin: "*",
+  };
+
+app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, "/ui/build")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/ui/build/index.html"));
+});
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
-app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
+app.listen(PORT, () =>{
+    connectDatabase();
+ console.log(`server running on port: ${PORT}`)
+})
